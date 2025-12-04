bb7f9efe4289e54fbf1d7e9323ff06ded8d8d436
Fredrik Robertsen <fredrikrobertsen7@gmail.com>
2025-12-04 15:03:58 +0100

    Define custom key combos in custom_config.h
    norwegian letters

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,24 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
+#define MIRYOKU_KLUDGE_CUSTOM_CONFIG \
+/ { \
+    combos { \
+        compatible = "zmk,combos"; \
+        combo_aa { \
+            timeout-ms = <50>; \
+            key-positions = <10 11>; \
+            bindings = <&kp RA(W)>; \
+        }; \
+        combo_oe { \
+            timeout-ms = <50>; \
+            key-positions = <11 12>; \
+            bindings = <&kp RA(L)>; \
+        }; \
+        combo_ae { \
+            timeout-ms = <50>; \
+            key-positions = <10 12>; \
+            bindings = <&kp RA(Z)>; \
+        }; \
+    }; \
+};
 
